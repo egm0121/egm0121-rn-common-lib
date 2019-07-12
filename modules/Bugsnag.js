375a270e3b5f4a24a08da1b3447227ff5b57dcd1
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2019-07-12 10:58:46 +0200

    enhance axios error paylod for bugsnag

--- modules/Bugsnag.js
+++ modules/Bugsnag.js
@@ -1,8 +1,28 @@
-import { Client } from 'bugsnag-react-native';
+import { Client, Configuration } from 'bugsnag-react-native';
 
 export default {
   init(client_id){
-    this.client = new Client(client_id);
+    const bsConfiguration = new Configuration();
+    bsConfiguration.apiKey = client_id;
+    bsConfiguration.registerBeforeSendCallback((report, error) => {
+      try {
+        if (error.config && typeof error.config === 'object') {
+          // eslint-disable-next-line no-param-reassign
+          report.metadata = {
+            axios: {
+              url: error.config.url,
+              params: error.config.params,
+              method: error.config.method,
+            },
+          };
+        }
+      } catch (err) {
+        console.log('bugsnag failed enhancing of error');
+        return true;
+      }
+      return true;
+    });
+    this.client = new Client(bsConfiguration);
   },
   getClient(){
    return this.client; 
