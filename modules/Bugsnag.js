83a5e82ab7b1cd4a38e4111d25e85a320a90d729
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2019-07-31 21:42:46 -0400

    addded setErrorMetadataCallback

--- modules/Bugsnag.js
+++ modules/Bugsnag.js
@@ -4,28 +4,37 @@ export default {
   init(client_id){
     const bsConfiguration = new Configuration();
     bsConfiguration.apiKey = client_id;
-    bsConfiguration.registerBeforeSendCallback((report, error) => {
-      try {
-        if (error.config && typeof error.config === 'object') {
-          // eslint-disable-next-line no-param-reassign
-          report.metadata = {
-            axios: {
-              url: error.config.url,
-              params: error.config.params,
-              method: error.config.method,
-            },
-          };
-        }
-      } catch (err) {
-        console.log('bugsnag failed enhancing of error');
-        return true;
-      }
-      return true;
-    });
+    bsConfiguration.registerBeforeSendCallback(this.enhanceErrorReport.bind(this));
     this.client = new Client(bsConfiguration);
+    this.errorMetadataCb = () => ({});
+  },
+  enhanceErrorReport(report, error) {
+    try {
+      report.metadata = report.metadata || {}
+      // handle axios failed request errors
+      if (error.config && typeof error.config === 'object') {
+        // eslint-disable-next-line no-param-reassign
+        report.metadata.axios = {
+          url: error.config.url,
+          params: error.config.params,
+          method: error.config.method,
+        };
+      }
+      report.metadata = {
+        ...report.metadata,
+        ...this.errorMetadataCb(report, error)
+      } 
+    } catch (err) {
+      console.log('bugsnag failed enhancing of error');
+      return true;
+    }
+    return true;
   },
   getClient(){
    return this.client; 
+  },
+  setErrorMetadataCallback(fn){
+    this.errorMetadataCb = fn;
   },
   notify(...args){
     return this.client.notify(...args);
